a741f317715be6e634c336c4dbbaba549a8cc479
Sagun Bastola <sagunbastola34@gmail.com>
2025-09-25 20:58:35 +0545

    new_commit

---
+++ search_in_2d_array_ii.cpp
@@ -0,0 +1,27 @@
+#include<vector>
+using namespace std;
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int m=matrix.size();
+        int n=matrix[0].size();
+        int r=0; int c=n-1;
+        while(r<m && c>=0)
+        {
+            if(target==matrix[r][c])
+            {
+                return true;
+            }
+            else  if(target>matrix[r][c])
+            {
+                r++;
+            }
+            else
+            {
+                c--;
+            }
+        }
+        return false;
+        
+    }
+};
