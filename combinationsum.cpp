ef834a36bb2e2eb4b9e099633acbbea2f0adb9d8
Sagun Bastola <sagunbastola34@gmail.com>
2025-10-01 12:18:13 +0545

    new_Co

--- combinationsum.cpp
+++ combinationsum.cpp
@@ -1,28 +1,36 @@
-class Solution {
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+#include<set>
+using namespace std;
+
+class Solution
+{
 public:
-    void helper(vector<int>& candidates,int sum,int target,vector<vector<int>>& ans,vector<int>& arr)
+    void helper(vector<int> &candidates, int sum, int target, vector<vector<int>> &ans, vector<int> &arr,int idx)
     {
-        if(sum==target)
+        if (sum == target)
         {
-            ans.push_back(arr)
+            ans.push_back(arr);
             return;
         }
-        if(sum>target)
+        if (sum > target)
         {
             return;
         }
-        for(int i=0;i<candidates.size(),i++)
+        for (int i = idx; i < candidates.size(); i++)
         {
-            sum+=candidates[i];
             arr.push_back(candidates[i]);
-            helper(candidates,sum,target,ans,arr);
+            helper(candidates, sum+candidates[i], target, ans, arr,i);
             arr.pop_back();
         }
     }
-    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+    vector<vector<int>> combinationSum(vector<int> &candidates, int target)
+    {
         vector<vector<int>> ans;
         vector<int> arr;
-        helper(candidates,0,target,ans,arr);
-        rerturn ans;
+        helper(candidates, 0, target, ans, arr,0);
+        return ans;
     }
 };
