e8761488e4cb6b1d67f00b02c2d3bb7f6c2ce99b
Sagun Bastola <sagunbastola34@gmail.com>
2025-10-05 11:17:06 +0545

    reverselinkedlistin pairs.cpp

---
+++ reverselinkedlistinkpairs.cpp
@@ -0,0 +1,52 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+#include <iostream>
+#include <list>
+struct ListNode
+{
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+using namespace std;
+
+class Solution {
+public:
+    ListNode* reverseKGroup(ListNode* head, int k) {
+        ListNode* temp=head;
+        int count=0;
+        while(count<k)
+        {
+            if(temp==NULL)
+            {
+                return head;
+
+            }
+            temp=temp->next;
+            count++;
+        }
+        count=0;
+        ListNode* prev=reverseKGroup(temp,k);
+        temp=head;
+        while(count<k)
+        {
+            ListNode* next=temp->next;
+            temp->next=prev;
+            prev=temp;
+            temp=next;
+            count++;
+        }
+        return prev;
+
+    }
+};
