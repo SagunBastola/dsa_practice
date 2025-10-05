a68b2d4ba00f14a1f9e6a27b4a7007baa5e94c0f
Sagun Bastola <sagunbastola34@gmail.com>
2025-10-05 15:38:50 +0545

    stackstock

--- stockspan.cpp
+++ stockspan.cpp
@@ -1,2 +1,31 @@
 #include<iostream>
+#include<vector>
+#include<stack>
 using namespace std;
+int main()
+{   
+    vector<int> arr={100,80,60,70,60,75,85};
+    vector<int> ans;
+    stack<int> s;
+    for(int i=0;i<arr.size();i++)
+    {
+        while(s.size()>0 && arr[s.top()]<=arr[i])
+        {
+            s.pop();
+        }
+        if(s.size()==0)
+        {
+            ans.push_back(i+1);
+        }
+        else{
+            ans.push_back(i-s.top());
+        }
+        s.push(i);
+    }
+    for(int val: ans)
+    {
+        cout<<val<<" ";
+    }
+
+    return 0;
+}
