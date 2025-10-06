b504e346032f125d2ac0d2c24085fa19ad7c98ab
Sagun Bastola <sagunbastola34@gmail.com>
2025-10-06 12:44:56 +0545

    histogramarea

---
+++ maxareahistogram.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    int largestRectangleArea(vector<int>& heights) {
+        stack<int> s;
+        int n=heights.size();
+        vector<int> right(n,-1);
+        vector<int> left(n,-1);
+        for(int i=n-1;i>=0 ;i--)
+        {
+            if(s.size()>0 && heights[s.top()]>=heights[i])
+            {
+                s.pop();
+            }
+            rights[i]=s.empty() ? n : s.top();
+            s.push(i);
+        }
+        while(!s.empty())
+        {
+            s.pop();
+        }
+        for(int i=0;i<n ;i++)
+        {
+            if(s.size()>0 && heights[s.top()]>=heights[i])
+            {
+                s.pop();
+            }
+            left[i]=s.empty() ? n : s.top();
+            s.push(i);
+        }
+        int ans=0;
+        for(int i=0;i<n;i++)
+        {
+            int width=right[i]-left[i];
+            int area=heights[i]*width;
+            ans=max(ans,area);
+        }
+        return ans;
+    }
+};
