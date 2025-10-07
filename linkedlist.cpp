07754878e2f58598e86b02fd056e59c1dc13bd95
Sagun Bastola <sagunbastola34@gmail.com>
2025-10-07 20:01:49 +0545

    linkedlist.cpp

--- linkedlist.cpp
+++ linkedlist.cpp
@@ -18,36 +18,60 @@ struct ListNode
     ListNode(int x) : val(x), next(nullptr) {}
     ListNode(int x, ListNode *next) : val(x), next(next) {}
 };
-class Solution {
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution
+{
 public:
-    ListNode* removeNthFromEnd(ListNode* head, int n) {
-        int count=0;
-        ListNode* temp=head;
-        while(temp->next!=NULL)
+    ListNode *removeNthFromEnd(ListNode *head, int n)
+    {
+        if (head == NULL)
         {
-            count+=1;
-            temp=temp->next;
+            return NULL;
         }
-        temp=head;
-        int nth=count-n;
-        count=0;
-        while(count!=(nth-1))
+        int count = 0;
+        ListNode *temp1 = head;
+        while (temp1->next != NULL)
         {
-            temp=temp->next;
-            
-            count+=1;
+            count += 1;
+            temp1 = temp1->next;
         }
-        if(count==(nth-1))
+        if (count > n)
         {
-            ListNode* prev=temp;
-            ListNode* curr=temp->next;
-            
-            ListNode* next=curr->next;
-            curr->next=NULL;
-            prev->next=next;
-            delete curr;
+            return NULL;
+        }
+        ListNode *temp = head;
+        int nth = count - n;
+        count = 0;
+        while (count != (nth - 1))
+        {
+            temp = temp->next;
 
+            count += 1;
+        }
+
+        if (count == (nth - 1))
+        {
+            ListNode *prev = temp;
+            ListNode *curr = temp->next;
+
+            ListNode *next = curr->next;
+            curr->next = NULL;
+            prev->next = next;
+            delete curr;
         }
         return head;
     }
 };
+int main()
+{
+    return 0;
+}
