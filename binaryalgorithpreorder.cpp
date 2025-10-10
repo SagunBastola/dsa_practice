4557c6ff7be30a7e208436d0affbd11dc531e775
Sagun Bastola <sagunbastola34@gmail.com>
2025-10-10 19:24:25 +0545

    binarytreesalgorithm

--- binaryalgorithpreorder.cpp
+++ binaryalgorithpreorder.cpp
@@ -24,12 +24,43 @@ node* binarytree(vector<int> &preorder)
     return root;
     
 }
+void preorder(node* binary)
+{
+    if(binary==NULL) return;
+    cout<<binary->data<<" ";
+    preorder(binary->left);
+    preorder(binary->right);
+
+}
+void postorder(node* binary)
+{
+    if(binary==NULL) return;
+    
+    preorder(binary->left);
+    preorder(binary->right);
+    cout<<binary->data<<" ";
+    
+}
+void inorder(node* binary)
+{
+    if(binary==NULL) return;
+    
+    preorder(binary->left);
+    cout<<binary->data<<" ";
+    preorder(binary->right);
+    
+    
+}
+
 int main()
 {
     vector<int> presequence={1,2,-1,-1,3,4,-1,-1,5,-1,-1};
     node* binary=binarytree(presequence);
-    cout<<binary->data<<" ";
-    cout<<binary->left->data<<" ";
-    cout<<binary->right->data<<" ";
+    preorder(binary);
+    cout<<endl;
+    postorder(binary);
+    cout<<endl;
+    inorder(binary);
+
     return 0;
 }
