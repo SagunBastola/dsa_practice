e906dfb3ca11aa8a2112daa03e041ff18aa5bde9
Sagun Bastola <sagunbastola34@gmail.com>
2025-10-16 23:41:27 +0545

    binarytree

---
+++ rightview.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    vector<int> rightSideView(TreeNode* root) {
+        vector<int> result;
+        if (!root) return result; 
+
+        queue<TreeNode*> q;
+        q.push(root);
+
+        while (!q.empty()) {
+            int levelSize = q.size();
+            TreeNode* node = nullptr;
+
+            for (int i = 0; i < levelSize; ++i) {
+                node = q.front();
+                q.pop();
+
+                if (node->left) q.push(node->left);
+                if (node->right) q.push(node->right);
+            }
+
+            result.push_back(node->val);
+        }
+
+        return result;
+    }
+};
