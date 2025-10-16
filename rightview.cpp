cbea2aec892875d96a0b32cfd61218980399a1fd
Sagun Bastola <sagunbastola34@gmail.com>
2025-10-16 23:55:04 +0545

    binarytree

--- rightview.cpp
+++ rightview.cpp
@@ -1,34 +1,53 @@
+#include <iostream>
+#include <map>
+#include <queue>
+#include <vector>
+using namespace std;
+struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
 class Solution
 {
 public:
     vector<int> rightSideView(TreeNode *root)
     {
         vector<int> result;
-        if (!root)
-            return result;
-
-        queue<TreeNode *> q;
-        q.push(root);
-
-        while (!q.empty())
+        if (root == 0)
         {
-            int levelSize = q.size();
-            TreeNode *node = nullptr;
-
-            for (int i = 0; i < levelSize; ++i)
-            {
-                node = q.front();
-                q.pop();
-
-                if (node->left)
-                    q.push(node->left);
-                if (node->right)
-                    q.push(node->right);
-            }
-
-            result.push_back(node->val);
+            return result;
         }
-
+        map<int, int> m;                // vertical distance and val
+        queue<pair<TreeNode *, int>> q; // node* and its vertical distance
+        q.push({root, 0});
+        int distance;
+        while (q.size() > 0)
+        {
+            TreeNode *curr = q.front().first;
+            distance = q.front().second;
+            if (m.find(distance) == m.end())
+            {
+                m[distance] = curr->val;
+            }
+            if (curr->right != NULL)
+            {
+                q.push({curr->right, distance + 1});
+            }
+            if (curr->left != NULL)
+            {
+                q.push({curr->left, distance + 1});
+            }
+            
+        }
+        for(auto v : m)
+        {
+            result.push_back(v.second);
+        }
         return result;
     }
 };
--- topview.cpp
+++ topview.cpp
@@ -1,6 +1,6 @@
-#include<iostream>
-#include<map>
-#include<queue>
+#include <iostream>
+#include <map>
+#include <queue>
 using namespace std;
 struct TreeNode
 {
@@ -11,17 +11,36 @@ struct TreeNode
     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
 };
-void topview(TreeNode* root)
+void topview(TreeNode *root)
 {
-    if(root==0)
+    if (root == 0)
     {
         return;
     }
-    map<int,int> m;//horizontal distance and val
-    queue<pair<TreeNode*,int>> q;//node* and its horizontal distance
-    q.push({root,0});
-    while(q.size()>0)
+    map<int, int> m;                // horizontal distance and val
+    queue<pair<TreeNode *, int>> q; // node* and its horizontal distance
+    q.push({root, 0});
+    int distance;
+    while (q.size() > 0)
     {
-
+        TreeNode *curr = q.front().first;
+        distance = q.front().second;
+        q.pop();
+        if (m.find(distance) == m.end())
+        {
+            m[distance] = curr->val;
+        }
+        if (curr->left != NULL)
+        {
+            q.push({curr->left, distance - 1});
+        }
+        if (curr->right != NULL)
+        {
+            q.push({curr->right, distance - 1});
+        }
+    }
+    for (auto m1 : m)
+    {
+        cout << m1.second << endl;
     }
 }
