3ab5e84a6046be5968a2dfead50f20d5a056171b
Sagun Bastola <sagunbastola34@gmail.com>
2025-11-29 21:43:43 +0545

    new

---
+++ stackusinglinkedlist.cpp
@@ -0,0 +1,91 @@
+#include<iostream>
+using namespace std;
+class node
+{
+public:
+    int val;
+    node *next;
+    node *prev;
+
+    node(int a) : val(a), next(NULL), prev(NULL)
+    {
+    }
+};
+class list
+{
+private:
+    node *head;
+    node *tail;
+
+public:
+    list()
+    {
+        head = tail = NULL;
+    }
+    void push_front(int val)
+    {
+        if (head == NULL)
+        {
+            node *newnode = new node(val);
+            head = tail = newnode;
+            return;
+        }
+        else
+        {
+            node *newnode = new node(val);
+            newnode->next = head;
+            head = newnode;
+            return;
+        }
+    }
+    void push_back(int val)
+    {
+        node* newnode=new node(val);
+        if(head==NULL)
+        {
+            head = tail = newnode;
+        }
+        else
+        {
+            tail->next=newnode;
+            tail=newnode;
+        }
+    }
+    void print_list()
+    {
+        node* temp;
+        temp=head;
+        while(temp!=NULL)
+        {
+            cout<<temp->val<<" ";
+            temp=temp->next;
+        }
+        cout<<endl;
+    }
+    void insert(int val,int pos)
+    {
+        if(pos<0)
+        {
+            cout<<"INVALID \n"<<endl;
+            return;
+        }
+        if(pos==0)
+        {
+            push_front(val);
+            return;
+        }
+        node* temp=head;
+        for(int i=0;i<pos-1;i++)
+        {
+            if(temp==NULL)
+            {
+                cout<<"invalid position \n";
+                return;
+            }
+            temp=temp->next;
+        }
+        node* newnode=new node(val);
+        newnode->next=temp->next;
+        temp->next=newnode;
+    }
+};
