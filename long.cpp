7f7c7b3784d7adfab0ff46e2df7687f28475fba6
Sagun Bastola <sagunbastola34@gmail.com>
2025-12-25 23:05:35 +0545

    commited

---
+++ long.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Solution
+{
+public:
+    int longestSubarray(vector<int> nums, int k)
+    {
+        int len=0;
+        int s=0;
+        int n=nums.size();
+        for(int i=0;i<n;i++)
+        {   s=0;
+            for(int j=i;j<n;j++)
+            {
+                s+=nums[j];
+                if(s==k)
+                {
+                    len=max(len,j-i+1);
+                }
+            }
+        }
+        return len;
+    }
+};
+int main()
+{
+    vector<int> nums = {1,-1,1};
+    int k = 1;
+
+    // Creating an object of Solution class
+    Solution sol;
+
+    /* Function call to find the length
+    of longest subarray having sum k */
+    int ans = sol.longestSubarray(nums, k);
+
+    cout << "The length of longest subarray having sum k is: " << ans;
+
+    return 0;
+}
--- longestsubarray.cpp
+++ longestsubarray.cpp
@@ -1,18 +1,47 @@
 #include <bits/stdc++.h>
 using namespace std;
-
-int main() {
-	vector<int> nums = {10, 5, 2, 7, 1, 9};
+class Solution
+{
+public:
+    int longestSubarray(vector<int> nums, int k)
+    {
+        int right = 0;
+        int left = 0;
+        int n = nums.size();
+        int maxlength = 0;
+        int sum = nums[0];
+        while (right < n)
+        {
+            if (sum > k)
+            {
+                sum -= nums[left];
+                left++;
+            }
+            if (sum == k)
+            {
+                maxlength = max(maxlength, right - left + 1);
+            }
+            right++;
+            if (right >= n)
+                break;
+            sum += nums[right];
+        }
+        return maxlength;
+    }
+};
+int main()
+{
+    vector<int> nums = {10, 5, 2, 7, 1, 9};
     int k = 15;
-    
-	// Creating an object of Solution class
-	Solution sol;
 
-	/* Function call to find the length
-	of longest subarray having sum k */
-	int ans = sol.longestSubarray(nums, k);
+    // Creating an object of Solution class
+    Solution sol;
 
-	cout << "The length of longest subarray having sum k is: " << ans;
+    /* Function call to find the length
+    of longest subarray having sum k */
+    int ans = sol.longestSubarray(nums, k);
 
-	return 0;
+    cout << "The length of longest subarray having sum k is: " << ans;
+
+    return 0;
 }
