d872069df587a9363535254169c695eaf6636bdf
Sagun Bastola <sagunbastola34@gmail.com>
2025-12-31 11:45:16 +0545

    contest1

---
+++ context1.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t;
+    cin >> t;
+    for (int i = 0; i < t; i++)
+    {
+        int n;
+        cin >> n;
+        string s;
+        cin >> s;
+        if (s.find("2026") < s.length())
+        {
+            cout << 0 << endl;
+        }
+        else if (!(s.find("2025") < s.length()))
+        {
+            cout << 0 << endl;
+        }
+        else
+        {
+            cout << 1 << endl;
+        }
+    }
+}
