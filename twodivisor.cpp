ca78cac2ae75f7c22d2cfd8a2b572bf9253a8963
Sagun Bastola <sagunbastola34@gmail.com>
2026-01-02 20:29:35 +0545

    two

--- twodivisor.cpp
+++ twodivisor.cpp
@@ -1,15 +1,26 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 int main()
 {
     int t;
-    cin>>t;
-    while(t--)
+    cin >> t;
+    while (t--)
     {
-        int a;
-        int b;
-        cin>>a;
-        cin>>b;
-        
+        long long int a;
+        long long int b;
+        cin >> a;
+        cin >> b;
+        if(b%a!=0)
+        {
+            long long g=__gcd(a,b);
+            long long x=(a/g)*b;
+            cout<<x<<endl;
+        }
+        else
+        {
+            long long mul=b/a;
+            long long x=b*mul;
+            cout<<x<<endl;
+        }
     }
 }
