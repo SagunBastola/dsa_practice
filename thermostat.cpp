4f24f32a7884638a259ebc80035b18b4710a636e
Sagun Bastola <sagunbastola34@gmail.com>
2026-01-03 15:49:39 +0545

    thermo

--- thermostat.cpp
+++ thermostat.cpp
@@ -1,28 +1,33 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-int main()
-{
+
+int main() {
     int t;
-    cin>>t;
-    while(t--)
-    {
-        int l,r,x,a,b;
-        cin>>l>>r>>x;
-        cin>>a>>b;
-        if(a==b)
-        {
-            cout<<0<<endl;
-            continue;
+    cin >> t;
+    
+    while (t--) {
+        int l, r, x, a, b;
+        cin >> l >> r >> x;
+        cin >> a >> b;
+
+        if (a == b) {
+            cout << 0 << endl;
         }
-        if(abs(a-b)>=x)
-        {
-            cout<<1<<endl;
-            continue;
+        else if (abs(a - b) >= x) {
+            cout << 1 << endl;
         }
-        if(abs(l-r)<=x)
-        {
-            cout<<-1<<endl;
-            continue;
+        else if (abs(a - r) < x && abs(a - l) < x || (abs(b-r)<x && abs(b-l)<x)) {
+            cout << -1 << endl;
+        }
+        else {
+            if ((abs(b-l) >= x && abs(a-l)>=x) || (abs(b-r)>=x && abs(a-r)>=x)) {
+                cout << 2 << endl;
+            }
+            else {
+                cout << 3 << endl;
+            }
         }
     }
+
+    return 0;
 }
